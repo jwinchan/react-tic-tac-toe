9347d52bb1a4ddf1b677b57515b06a958b694da7
Sandy <sandyvasquez9@gmail.com>
2020-12-29 20:30:06 -0800

    added onlick to the square button

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,16 +4,15 @@ import PropTypes from 'prop-types';
 import './Square.css'
 
 const Square = (props) => {
-  // For Wave 1 enable this 
-  //  Component to alert a parent 
-  //  component when it's clicked on.
 
   return <button
     className="square"
+    onClick={props.onClickCallback}
   >
     {props.value}
   </button>
 }
+
 
 Square.propTypes = {
   value: PropTypes.string.isRequired,
